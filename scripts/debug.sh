a1482ec5048da7b1a6fdb046ac5ee9c115424630
james.goppert <james.goppert@2d985f29-a1c8-afb7-27ee-997f99665c14>
2011-04-01 13:05:22 +0000

    Removed extra files.
    git-svn-id: https://ardupilotone.googlecode.com/svn/trunk@37 2d985f29-a1c8-afb7-27ee-997f99665c14

--- scripts/debug.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-avarice --mkII --capture --jtag usb :4242 &
-gnome-terminal -x avr-gdb /tmp/ArduPilotOne.build/ArduPilotOne.elf
--- scripts/upload.sh
+++
@@ -1,6 +0,0 @@
-#!/binb/bash
-#port = $(zenity --entry --text "port"  --title "AVR Upload")
-port=$1
-avrdude -c arduino -p m1280 -P $port -carduino -b57600 -U /tmp/ArduPilotOne.build/ArduPilotOne.hex
-
-
