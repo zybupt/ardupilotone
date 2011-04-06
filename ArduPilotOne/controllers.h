ca3255730e58c627e7cd498c453697ca82792295
james.goppert <james.goppert@2d985f29-a1c8-afb7-27ee-997f99665c14>
2011-04-06 01:26:50 +0000

    Finished quad controller.
    git-svn-id: https://ardupilotone.googlecode.com/svn/trunk@125 2d985f29-a1c8-afb7-27ee-997f99665c14

--- ArduPilotOne/controllers.h
+++ ArduPilotOne/controllers.h
@@ -6,7 +6,6 @@
 #include "AP_RcChannel.h"
 #include "AP_Var.h"
 #include <avr/pgmspace.h>
-
 #include "AP_Navigator.h"
 
 namespace apo {
@@ -90,6 +89,48 @@ public:
 		}
 		virtual void update(const float & dt) {
 			_controller->_attitudeTiltMax = 1;
+			/*
+			// inputs
+			//_controller->commandNorthTilt;
+			//_controller->commandEastTilt;
+			//_controller->commandThrustMix;
+			//_controller->commandYawMix;
+
+			// float yaw = _controller->_nav->yaw;
+
+			// "transform-to-body"
+			{
+				float trigSin = sin(_controller->_nav->yaw);
+				float trigCos = cos(_controller->_nav->yaw);
+				_controller->commandPitch = _controller->commandEastTilt * trigCos +
+						_controller->commandNorthTilt * trigSin;
+				_controller->commandRoll =  _controller->commandEastTilt * trigSin -
+						_controller->commandNorthTilt * trigCos;
+				// note that the north tilt is negative of the pitch
+			}
+
+			// "thrust-trim-adjust"
+			if (fabs(_controller->commandRoll) > 0.5) {
+				_controller->thrustMix *= 1.13949393;
+			} else {
+				_controller->thrustMix /= cos(_controller->commandRoll);
+			}
+			if (fabs(_controller->commandPitch) > 0.5) {
+				_controller->thrustMix *= 1.13949393;
+			} else {
+				_controller->thrustMix /= cos(_controller->commandPitch);
+			}
+
+			// "mix manual"
+			_controller->commandRoll -= _ATT_OFFSET_X * _MIX_OFFSET_WEIGHT;
+			_controller->commandPitch -= _ATT_OFFSET_Y * _MIX_OFFSET_WEIGHT;
+			_controller->thrustMix -= _ATT_OFFSET_Z * _MIX_OFFSET_WEIGHT;
+
+			_controller->commandRoll += _controller->hal->rc[CH_ROLL]->getPosition() * _MIX_REMOTE_WEIGHT;
+			_controller->commandPitch += _controller->hal->rc[CH_PITCH]->getPosition() * _MIX_REMOTE_WEIGHT;
+			_controller->commandYaw += _controller->hal->rc[CH_YAW]->getPosition() * _MIX_REMOTE_WEIGHT;
+			_controller->thrustMix += _controller->hal->rc[CH_THR]->getPosition() * _MIX_REMOTE_WEIGHT;
+			*/
 		}
 	private:
 		QuadController * _controller;
@@ -211,164 +252,6 @@ float QuadController::_thrustMixMin = -1;
 float QuadController::_thrustMixMax = -1;
 float QuadController::_thrustMixTrim = 0.5;
 
-/*
-class QuadController: public AP_Controller {
-private:
-	// bridge variables
-	float commandRoll;
-	float commandPitch;
-	float commandYaw;
-
-	// mix variables
-	float thrustMix;
-	float rollMix;
-	float yawMix;
-	float pitchMix;
-
-	// motor channels
-//	AP_RcChannel * chMode;
-//	AP_RcChannel * chLeft;
-//	AP_RcChannel * chRight;
-//	AP_RcChannel * chFront;
-//	AP_RcChannel * chBack;
-//	AP_RcChannel * chRoll;
-//	AP_RcChannel * chPitch;
-//	AP_RcChannel * chYaw;
-//	AP_RcChannel * chThr;
-
-
-public:
-	QuadController(AP_Var::Key cntrlKey,
-			AP_Var::Key chLeftKey, AP_Var::Key chRightKey, AP_Var::Key chFrontKey, AP_Var::Key chBackKey,
-			AP_Var::Key chRollKey, AP_Var::Key chPitchKey, AP_Var::Key chYawKey, AP_Var::Key chThrKey,
-			AP_Var::Key pidPNKey, AP_Var::Key pidPEKey, AP_Var::Key pidPDKey,
-			AP_Var::Key pidRollKey, AP_Var::Key pidPitchKey, AP_Var::Key pidYawRateKey, AP_Var::Key pidYawKey,
-			aP_Navigator * nav, AP_Guide * guide, AP_HardwareAbstractionLayer * hal) : _navigator(nav), AP
-	{
-		Serial.println_P(PSTR("initializing quad controller"));
-
-		////
-		// POSITION CONTROL
-		//
-		// inputs:  reference north, reference east, reference down, reference yaw (user declared)
-		//	    output north, output east, output down			   (output declared)
-		//	    d/dt output north, d/dt output east, d/dt output down	   (feedback declared)
-		// outputs: commandNorth, commandEast, commandDown, commandYaw
-		//
-		////
-
-		addBlock(new SumGain(pNCmd,&one,pN,&negativeOne));
-		addBlock(new PidDFB(pidPNKey, PSTR("North"), vN, PID_POS_P,PID_POS_I,PID_POS_D,PID_POS_AWU));
-		addBlock(new Saturate(PID_POS_LIM_MIN,PID_POS_LIM));
-		addBlock(new Sink(commandNorth));
-
-		addBlock(new SumGain(pECmd,&one,pE,&negativeOne));
-		addBlock(new PidDFB(pidPEKey, PSTR("East"), vE, PID_POS_P,PID_POS_I,PID_POS_D,PID_POS_AWU));
-		addBlock(new Saturate(PID_POS_LIM_MIN,PID_POS_LIM));
-		addBlock(new Sink(commandEast));
-		//
-		//				addBlock(new SumGain(pDCmd,&one,pD,&negativeOne));
-		//				addBlock(new PidDFB(pidPDKey, PSTR("Down"),  vD, PID_POS_Z_P,PID_POS_Z_I,PID_POS_Z_D,PID_POS_Z_AWU));
-		//				addBlock(new Saturate(PID_POS_Z_LIM_MIN,PID_POS_Z_LIM));
-		//				addBlock(new Sink(commandDown));
-
-		////
-		// Bridge between POSITION and ATTITUDE control
-		//
-		// inputs:  (-commandNorth), commandEast, (-thrustMix), commandYaw
-		// outputs: commandRoll, commandPitch, commandYaw, thrustMix
-		//
-		////
-
-		addBlock(new Bridge(&commandNorth,&commandEast,&commandDown,&commandYaw));
-
-		////
-		// ATTITUDE CONTROL
-		//
-		// inputs:  commandRoll, commandPitch, commandYaw
-		// outputs: rollMix, pitchMix, yawMix
-		// note:    thrustMix (untouched)
-		//
-		////
-
-		addBlock(new SumGain(&commandRoll,&one,roll,&negativeOne));
-		addBlock(new PidDFB(pidRollKey, PSTR("ROLL"), rollRate, PID_ATT_P, PID_ATT_I, PID_ATT_D, PID_ATT_AWU));
-		addBlock(new Sink(rollMix));
-
-		addBlock(new SumGain(&commandPitch,&one,pitch,&negativeOne));
-		addBlock(new PidDFB(pidPitchKey, PSTR("PITCH"), pitchRate, PID_ATT_P,PID_ATT_I,PID_ATT_D,PID_ATT_AWU));
-		addBlock(new Sink(pitchMix));
-
-		addBlock(new SumGain(&commandYaw,&one,yaw,&negativeOne));
-		addBlock(new PidDFB(pidYawRateKey, PSTR("YAWrate"), yawRate, PID_YAWPOS_P,PID_YAWPOS_I,PID_YAWPOS_D,PID_YAWPOS_AWU));
-		addBlock(new Pid(pidYawKey, PSTR("YAW"), PID_YAWSPEED_P,PID_YAWSPEED_I,PID_YAWSPEED_D,PID_YAWSPEED_AWU));
-		addBlock(new Sink(yawMix));
-
-		////
-		// THRUST TRIM
-		//
-		// inputs:  thrustMix
-		// outputs: thrustMix
-		// notes: rollMix, pitchMix, yawMix (untouched)
-		//
-		////
-
-		addBlock(new SumGain(THRUST_HOVER_OFFSET,&one,&thrustMix,&one));
-		addBlock(new Sink(thrustMix));
-
-		////
-		// MOTOR MIX
-		//
-		// inputs:  rollMix, pitchMix, yawMix, thrustMix
-		// outputs: (to servo: chLeft, chRight, chFront, chBack)
-		//
-		////
-
-		// Left Motor
-		addBlock(new SumGain(&thrustMix, &one, &rollMix, &one, &yawMix, &one));
-		addBlock(new Saturate(motorMin,motorMax));
-		addBlock(new ToServo(chLeft));
-
-		// Right Motor
-		addBlock(new SumGain(&thrustMix,&one,&rollMix,&negativeOne,&yawMix,&one));
-		addBlock(new Saturate(motorMin,motorMax));
-		addBlock(new ToServo(chRight));
-
-		// Front Motor
-		addBlock(new SumGain(&thrustMix,&one,&pitchMix,&one,&yawMix,&negativeOne));
-		addBlock(new Saturate(motorMin,motorMax));
-		addBlock(new ToServo(chFront));
-
-		// Back Motor
-		addBlock(new SumGain(&thrustMix,&one,&pitchMix,&negativeOne,&yawMix,&negativeOne));
-		addBlock(new Saturate(motorMin,motorMax));
-		addBlock(new ToServo(chBack));
-	}
-
-};
-void controllerInit() {
-	_rc.push_back(new AP_RcChannel(k_chMode, PSTR("MODE_"), APM_RC, 7, 45));
-	_rc.push_back(new AP_RcChannel(k_chLeft, PSTR("LEFT_"), APM_RC, 0, 1));
-	_rc.push_back(new AP_RcChannel(k_chRight, PSTR("RIGHT_"), APM_RC, 1, 1));
-	_rc.push_back(new AP_RcChannel(k_chFront, PSTR("FRONT_"), APM_RC, 2, 1));
-	_rc.push_back(new AP_RcChannel(k_chBack, PSTR("BACK_"), APM_RC, 3, 1));
-
-	_controller = new QuadController(k_cntrl,
-			k_chLeft, k_chRight, k_chFront, k_chBack,
-			k_chRoll, k_chPitch, k_chYaw, k_chThr,
-			k_pidPN, k_pidPE, k_pidPD,
-			k_pidRoll, k_pidPitch, k_pidYawRate, k_pidYaw,
-			&navigator()->groundSpeed, &guide()->groundSpeedCommand,
-			&navigator()->yaw, &guide()->headingCommand,
-			&navigator()->roll, &navigator()->pitch, &navigator()->yaw,
-			&navigator()->rollRate, &navigator()->pitchRate, &navigator()->yawRate,
-			&guide()->pNCmd, &navigator()->pN, &navigator()->vN,
-			&guide()->pECmd, &navigator()->pE, &navigator()->vE,
-			&guide()->pDCmd, &navigator()->pD, &navigator()->vD,
-			_rc[0], _rc[1], _rc[2], _rc[3], _rc[4],
-			_rc[5], _rc[6], _rc[7], _rc[8]);
-}
-*/
 
 /*
 class PlaneController : public AP_Controller
@@ -428,83 +311,6 @@ public:
 	}
 };
 */
-
-////
-// AP_Controller.h extended: functions for "BRIDGE", "PIDDFB", and "QUADMIX"
-////
-
-/// Bridge Block
-/**
- * Bridge between POSITION and ATTITUDE control
- *
- * inputs:  commandNorth, commandEast, thrustMix, commandYaw
- * outputs: commandRoll, commandPitch, commandYaw, thrustMix
- */
-/*
-class Bridge: public AP_Controller::Block {
-public:
-	// TODO switch to quad controller if needed
-	Bridge(AP_Controller * controller) :
-		_controller(controller)
-	{
-	}
-	virtual void update(const float & dt) {
-
-		// inputs
-		//_controller->commandNorthTilt;
-		//_controller->commandEastTilt;
-		//_controller->commandThrustMix;
-		//_controller->commandYawMix;
-
-		// float yaw = _controller->_nav->yaw;
-
-		// "transform-to-body"
-		{
-			float trigSin = sin(_controller->_nav->yaw);
-			float trigCos = cos(_controller->_nav->yaw);
-			_controller->commandPitch = _controller->commandEastTilt * trigCos +
-					_controller->commandNorthTilt * trigSin;
-			_controller->commandRoll =  _controller->commandEastTilt * trigSin -
-					_controller->commandNorthTilt * trigCos;
-			// note that the north tilt is negative of the pitch
-		}
-
-		// "thrust-trim-adjust"
-		if (fabs(_controller->commandRoll) > 0.5) {
-			_controller->thrustMix *= 1.13949393;
-		} else {
-			_controller->thrustMix /= cos(_controller->commandRoll);
-		}
-		if (fabs(_controller->commandPitch) > 0.5) {
-			_controller->thrustMix *= 1.13949393;
-		} else {
-			_controller->thrustMix /= cos(_controller->commandPitch);
-		}
-
-		// "mix manual"
-		_controller->commandRoll -= _ATT_OFFSET_X * _MIX_OFFSET_WEIGHT;
-		_controller->commandPitch -= _ATT_OFFSET_Y * _MIX_OFFSET_WEIGHT;
-		_controller->thrustMix -= _ATT_OFFSET_Z * _MIX_OFFSET_WEIGHT;
-
-		_controller->commandRoll += _controller->hal->rc[CH_ROLL]->getPosition() * _MIX_REMOTE_WEIGHT;
-		_controller->commandPitch += _controller->hal->rc[CH_PITCH]->getPosition() * _MIX_REMOTE_WEIGHT;
-		_controller->commandYaw += _controller->hal->rc[CH_YAW]->getPosition() * _MIX_REMOTE_WEIGHT;
-		_controller->thrustMix += _controller->hal->rc[CH_THR]->getPosition() * _MIX_REMOTE_WEIGHT;
-	}
-private:
-	float * _cNorth;
-	float * _cEast;
-	float * _cDown;
-	float * _cYaw;
-	AP_RcChannel * _chRoll;
-	AP_RcChannel * _chPitch;
-	AP_RcChannel * _chYaw;
-	AP_RcChannel * _chThr;
-	AP_Controller * _controller;
-
-};
-*/
-
 
 } // namespace apo
 
