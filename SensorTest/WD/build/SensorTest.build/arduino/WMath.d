a93259b6cd972d726aff7bf26a33bde40df281ab
james.goppert <james.goppert@2d985f29-a1c8-afb7-27ee-997f99665c14>
2011-04-09 18:42:59 +0000

    Removed extra folder.
    git-svn-id: https://ardupilotone.googlecode.com/svn/trunk@163 2d985f29-a1c8-afb7-27ee-997f99665c14

--- SensorTest/WD/build/SensorTest.build/arduino/WMath.d
+++
@@ -1,5 +0,0 @@
- \
- /hsl/homes/jgoppert/Projects/ardupilotone/ArduPilotOne/SensorTest/WD/build/SensorTest.build/arduino/WMath.o:  \
- /usr/share/arduino/hardware/arduino/cores/arduino/WMath.cpp \
-  /usr/lib/gcc/avr/4.3.5/../../../avr/include/stdlib.h \
-  /usr/lib/gcc/avr/4.3.5/include/stddef.h
