9d594cd82127ca0ad5d30fb2339d7e0976d83999
James Goppert <james.goppert@gmail.com>
2011-09-26 10:28:58 -0400

    Added tank.

---
+++ ardupilotone/ControllerTank.h
@@ -0,0 +1,123 @@
+/*
+ * ControllerTank.h
+ *
+ *  Created on: Jun 30, 2011
+ *      Author: jgoppert
+ */
+
+#ifndef CONTROLLERTANK_H_
+#define CONTROLLERTANK_H_
+
+#include "../APO/AP_Controller.h"
+
+namespace apo {
+
+class ControllerTank: public AP_Controller {
+private:
+	AP_Var_group _group;
+	AP_Uint8 _mode;
+	enum {
+		k_chMode = k_radioChannelsStart, k_chStr, k_chThr
+	};
+	enum {
+		k_pidStr = k_controllersStart, k_pidThr
+	};
+	enum {
+		CH_MODE = 0, CH_LEFT, CH_RIGHT
+	};
+	BlockPIDDfb pidStr;
+	BlockPID pidThr;
+public:
+	ControllerTank(AP_Navigator * nav, AP_Guide * guide,
+			AP_HardwareAbstractionLayer * hal) :
+				AP_Controller(nav, guide, hal),
+				_group(k_cntrl, PSTR("CNTRL_")),
+				_mode(&_group, 1, MAV_MODE_UNINIT, PSTR("MODE")),
+				pidStr(new AP_Var_group(k_pidStr, PSTR("STR_")), 1, steeringP,
+						steeringI, steeringD, steeringIMax, steeringYMax),
+				pidThr(new AP_Var_group(k_pidThr, PSTR("THR_")), 1, throttleP,
+						throttleI, throttleD, throttleIMax, throttleYMax,
+						throttleDFCut) {
+		_hal->debug->println_P(PSTR("initializing tank controller"));
+
+		_hal->rc.push_back(
+				new AP_RcChannel(k_chMode, PSTR("MODE_"), APM_RC, 7, 1100,
+						1500, 1900));
+		_hal->rc.push_back(
+				new AP_RcChannel(k_chStr, PSTR("LEFT_"), APM_RC, 0, 1100, 1540,
+						1900));
+		_hal->rc.push_back(
+				new AP_RcChannel(k_chThr, PSTR("RIGHT_"), APM_RC, 1, 1100, 1500,
+						1900));
+	}
+	virtual MAV_MODE getMode() {
+		return (MAV_MODE) _mode.get();
+	}
+	virtual void update(const float & dt) {
+
+		// check for heartbeat
+		if (_hal->heartBeatLost()) {
+			_mode = MAV_MODE_FAILSAFE;
+			setAllRadioChannelsToNeutral();
+			_hal->setState(MAV_STATE_EMERGENCY);
+			_hal->debug->printf_P(PSTR("comm lost, send heartbeat from gcs\n"));
+			return;
+		// if throttle less than 5% cut motor power
+		} else if (_hal->rc[CH_THR]->getRadioPosition() < 0.05) {
+			_mode = MAV_MODE_LOCKED;
+			setAllRadioChannelsToNeutral();
+			_hal->setState(MAV_STATE_STANDBY);
+			return;
+		// if in live mode then set state to active
+		} else if (_hal->getMode() == MODE_LIVE) {
+			_hal->setState(MAV_STATE_ACTIVE);
+		// if in hardware in the loop (control) mode, set to hilsim
+		} else if (_hal->getMode() == MODE_HIL_CNTL) {
+			_hal->setState(MAV_STATE_HILSIM);
+		}
+		
+		// read switch to set mode
+		if (_hal->rc[CH_MODE]->getRadioPosition() > 0) {
+			_mode = MAV_MODE_MANUAL;
+		} else {
+			_mode = MAV_MODE_AUTO;
+		}
+
+		// manual mode
+		switch (_mode) {
+
+		case MAV_MODE_MANUAL: {
+			setAllRadioChannelsManually();
+			break;
+		}
+		case MAV_MODE_AUTO: {
+			float headingError = _guide->getHeadingCommand()
+					- _nav->getHeading();
+			if (headingError > 180 * deg2Rad)
+				headingError -= 360 * deg2Rad;
+			if (headingError < -180 * deg2Rad)
+				headingError += 360 * deg2Rad;
+			float headingOuput = pidStr.update(headingError, _nav->getYawRate(), dt);
+			float throttleOutput = pidThr.update(_guide->getGroundSpeedCommand()
+									- _nav->getGroundSpeed(), dt);
+			_hal->rc[CH_LEFT]->setPosition(throttleOutput + headingOutput);
+			_hal->rc[CH_RIGHT]->setPosition(throttleOutput - headingOutput);
+			//_hal->debug->println("automode");
+			break;
+		}
+
+		default: {
+			setAllRadioChannelsToNeutral();
+			_mode = MAV_MODE_FAILSAFE;
+			_hal->setState(MAV_STATE_EMERGENCY);
+			_hal->debug->printf_P(PSTR("unknown controller mode\n"));
+			break;
+		}
+
+		}
+	}
+};
+
+} // namespace apo
+
+#endif /* CONTROLLERTANK_H_ */
